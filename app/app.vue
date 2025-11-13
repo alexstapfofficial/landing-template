280bb35ff822af6989ea575a01eadc7a371cd08f
Alexander Stapf <alexanderstapf@e90948e5-b258-460e-8582-752d9c714942.fritz.box>
2025-11-13 16:04:47 +0100

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    class: '',
+    lang: 'de'
+  },
+  script: [
+    { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+  ]
+})
+</script>
+
+<template>
+  <UApp :toaster="{ expand: false }">
+    <UMain class="relative">
+      <NuxtPage />
+    </UMain>
+  </UApp>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// nuxt.config.ts
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/content',
+  ],
+
+
+  css: ['~/assets/css/main.css'],
+
+})
